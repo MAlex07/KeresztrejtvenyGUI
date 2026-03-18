cedf53a00367590a4eb402e6a7d2481d7354f2ec
Petrik Diak <diak@petrik.hu>
2026-03-18 10:21:56 +0100

    a b) rész megvan

--- KeresztrejtvenyGUI/MainWindow.xaml.cs
+++ KeresztrejtvenyGUI/MainWindow.xaml.cs
@@ -37,5 +37,45 @@ namespace KeresztrejtvenyGUI
 
 
 		}
-    }
+
+		private void btn_letrehoz_Click(object sender, RoutedEventArgs e)
+		{
+            for(int i = 0; i<Rejtveny.Children.Count; i++)
+            {
+                if (Rejtveny.Children[i] is TextBox)
+                {
+                    Rejtveny.Children.Remove(Rejtveny.Children[i] as TextBox);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+			int sorokSzama = (int)cb_sorokszama.SelectedItem;
+            int oszlopokSzama = (int)cb_oszlopokszama.SelectedItem;
+
+
+				for (int r = 0; r<sorokSzama; r++)
+            {
+                for(int c = 0; c<oszlopokSzama; c++)
+                {
+                    TextBox aktualistb = new TextBox();
+
+                    aktualistb.Width = 15;
+                    aktualistb.Height = 15;
+                    aktualistb.TextAlignment = TextAlignment.Center;
+                    aktualistb.VerticalContentAlignment = VerticalAlignment.Center;
+                    aktualistb.MaxLength = 1;
+
+                    Canvas.SetLeft(aktualistb, c * 20);
+                    Canvas.SetTop(aktualistb, r * 20);
+
+                    Rejtveny.Children.Add(aktualistb);
+
+
+				}
+            }
+		}
+	}
 }
